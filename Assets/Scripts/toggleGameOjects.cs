eff873be044ce5958adb57305d7e9691fcc196aa
Akshat <akshatraj20105@gmail.com>
2024-04-18 19:08:47 +0530

    minor fixes and bgm added
    bgm can be changed by changing .mp3 file in xr rig audio source

--- Assets/Scripts/toggleGameOjects.cs
+++ Assets/Scripts/toggleGameOjects.cs
@@ -1,33 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR;
 
 public class toggleGameOjects : MonoBehaviour
 {
     public GameObject[] objectsToToggle; // Array of game objects to toggle
 
+   
+
     void Update()
     {
-        // Check if the left mouse button is clicked
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Cast a ray from the mouse position into the scene
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            // Check if the ray hits any game object
-            if (Physics.Raycast(ray, out hit))
-            {
-                // Check if the clicked game object has this script attached
-                toggleGameOjects toggleScript = hit.collider.GetComponent<toggleGameOjects>();
-
-                if (toggleScript != null)
-                {
-                    // Toggle the active state of the game objects
-                    ToggleObjects();
-                }
-            }
-        }
+     
     }
 
     public void ToggleObjects()
